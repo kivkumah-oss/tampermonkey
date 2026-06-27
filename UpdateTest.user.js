2b608bbe55a33c67220598fcc27d5911273b2db2
kivkumah-oss <kivkumah@gmail.com>
2026-06-27 04:12:38 +0100

    Create UpdateTest.user.js

---
+++ UpdateTest.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Update Test
+// @namespace    https://github.com/kivkumah-oss
+// @version      1.0
+// @description  Testing GitHub auto updates
+// @author       Martin
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    console.log("Update Test v1.0");
+
+    if (!document.getElementById("martin-update-test")) {
+
+        const div = document.createElement("div");
+
+        div.id = "martin-update-test";
+
+        div.style.position = "fixed";
+        div.style.bottom = "20px";
+        div.style.right = "20px";
+        div.style.padding = "12px 18px";
+        div.style.background = "#7c4dff";
+        div.style.color = "white";
+        div.style.borderRadius = "12px";
+        div.style.fontWeight = "bold";
+        div.style.zIndex = "999999";
+
+        div.textContent = "Version 1.0";
+
+        document.body.appendChild(div);
+
+    }
+
+})();
