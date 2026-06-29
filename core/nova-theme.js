1e2d3c3b11746ecd0a33f094369a7bf29f24d2d9
kivkumah-oss <kivkumah@gmail.com>
2026-06-29 07:15:10 +0100

    Update nova-theme.js

--- core/nova-theme.js
+++ core/nova-theme.js
@@ -1,1 +1,18 @@
+(function () {
+    'use strict';
 
+    window.NovaTheme = {
+
+        version: "0.1.0",
+
+        init() {
+            console.log("🎨 Nova Theme Engine loaded");
+        },
+
+        apply(theme) {
+            console.log("Applying theme:", theme);
+        }
+
+    };
+
+})();
