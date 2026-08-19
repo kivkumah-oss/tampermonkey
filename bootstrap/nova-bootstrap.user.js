b1287ed942e95c0c1243b4a9565622e21c57b485
kivkumah-oss <kivkumah@gmail.com>
2026-08-19 03:39:38 +0100

    Bump Nova Bootstrap to 2.3.1 for Rodeo modules

--- bootstrap/nova-bootstrap.user.js
+++ bootstrap/nova-bootstrap.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nova Core Bootstrap
 // @namespace    nova-core
-// @version      2.3.0
+// @version      2.3.1
 // @description  Install once. Nova Core, modules, updates, cache, recovery, and flicker-free site startup are managed automatically from GitHub.
 // @author       Martins + Nova
 // @match        *://*/*
@@ -19,6 +19,11 @@
 // @connect      *.cloudfront.net
 // @connect      cdn1.suno.ai
 // @connect      cdn-o.suno.com
+// @connect      *.amazon.com
+// @connect      *.a2z.com
+// @connect      amazon.co.uk
+// @connect      *.amazon.co.uk
+// @connect      localhost
 // @sandbox      JavaScript
 // @run-at       document-start
 // @noframes
@@ -32,7 +37,7 @@
   if (window.__NOVA_BOOTSTRAP_RUNNING__) return;
   window.__NOVA_BOOTSTRAP_RUNNING__ = true;
 
-  const VERSION = '2.3.0';
+  const VERSION = '2.3.1';
   const MANIFEST_URL = 'https://raw.githubusercontent.com/kivkumah-oss/tampermonkey/main/nova.manifest.json';
   const TRUSTED_PREFIX = 'https://raw.githubusercontent.com/kivkumah-oss/tampermonkey/';
   const PREFIX = 'nova.bootstrap.v2.';
